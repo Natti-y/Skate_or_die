1c7c578c095bd1179c31ab808bd1dc9099a836af
Johanna Bredinger <johanna.bredinger@gritacademy.se>
2025-12-18 16:25:48 +0100

    Added div and images to "new products"

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -3,6 +3,7 @@ import VideoPlayer from '../../shared/components/VideoPlayer';
 
 const Landing = () => {
   return (
+    <>
     <section className="landing landing-flex">
       
       <div className="landing-left">
@@ -19,6 +20,29 @@ const Landing = () => {
       </div>
 
     </section>
+
+
+    <section className="new-products">
+      <h2 className="new-products-title">New Drops</h2>
+
+      <div className="products-grid">
+        <div className="product-card">
+          <h3>T-shirt</h3>
+          <img src="src/assets/newDrops/skateordie-tshirt.jpg" alt="T-shirt" />
+        </div>
+
+        <div className="product-card">
+          <h3>Hoodie</h3>
+          <img src="src/assets/newDrops/skateordiehoodie.webp" alt="Hoodie" />
+        </div>
+
+        <div className="product-card">
+          <h3>Shoes</h3>
+          <img src="src/assets/newDrops/skateordieshoes.jpg" alt="Shoes" />
+        </div>
+      </div>
+    </section>
+    </>
   );
 };
 

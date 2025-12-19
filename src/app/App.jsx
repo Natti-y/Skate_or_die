6e45871d5699f5f2d6f31510d4990f721d720e92
MaryamRasouli526 <maryam.rasouli@gritacademy.se>
2025-12-19 01:02:55 +0100

    Create New Category + hamnar i New Categories listan
    1. Nu kan man skriva in en ny kategori och skapa det
    
    2. Den ny kategorin listas i dropdown menyn i "New Categories".

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react';
 import Navbar from '../shared/components/Navbar';
 import Landing from '../pages/landing/Landing';
 import Products from '../pages/products/Products';
+import CreateNewCategories from '../pages/createNewCategories/CreateNewCategories';
+
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [newCategories, setNewCategories] = useState([]); // Detta är lista för new categories
+
 
   const handleNavigate = (page, category = '') => {
     setCurrentPage(page);
@@ -14,9 +18,16 @@ function App() {
 
   return (
     <>
-      <Navbar onNavigate={handleNavigate} />
+      <Navbar onNavigate={handleNavigate} newCategories={newCategories} /> 
       {currentPage === 'home' && <Landing />}
       {currentPage === 'products' && <Products category={selectedCategory} />}
+      {currentPage === 'create-category' && (
+        <CreateNewCategories
+          newCategories={newCategories}      
+          setNewCategories={setNewCategories} 
+        />
+      )}
+
     </>
   );
 }
